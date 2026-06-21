a93e599ad8d4222c368a86d7afd3acd80caeb6f9
lukaswiltafsky <lukas@wiltafsky.de>
2026-06-21 13:21:32 +0200

    Alert Meldung entfernt beim Button klicken nach erflogreichem test

--- CO2_Website/js/script.js
+++ CO2_Website/js/script.js
@@ -1,12 +1,11 @@
 window.setDirection = function (direction) {
-    alert("Button wurde geklickt!");
-
+    
     document.documentElement.setAttribute("dir", direction);
     localStorage.setItem("direction", direction);
 };
 
 window.onload = function () {
-    
+
     const savedDirection = localStorage.getItem("direction");
 
     if (savedDirection) {
