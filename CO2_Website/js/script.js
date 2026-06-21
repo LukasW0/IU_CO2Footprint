7bd2521edbacca20837cdbc23850bcda38daaf67
lukaswiltafsky <lukas@wiltafsky.de>
2026-06-21 13:15:17 +0200

    Weitere Tests mit link rechts js einbindung

--- CO2_Website/js/script.js
+++ CO2_Website/js/script.js
@@ -1,10 +1,12 @@
-function setDirection(direction) {
+window.setDirection = function (direction) {
     alert("Button wurde geklickt!");
+
     document.documentElement.setAttribute("dir", direction);
-}
+    localStorage.setItem("direction", direction);
+};
 
 window.onload = function () {
-
+    
     const savedDirection = localStorage.getItem("direction");
 
     if (savedDirection) {
