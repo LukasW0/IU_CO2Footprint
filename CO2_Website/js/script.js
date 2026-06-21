73acaf94801c2f0ecfacc314654360d8f902ddac
lukaswiltafsky <lukas@wiltafsky.de>
2026-06-21 12:58:54 +0200

    Funktion hat noch nicht funktioniert. Test Meldung eingebaut um zu sehen ob JavaScript überhaupt geladen wird.

--- CO2_Website/js/script.js
+++ CO2_Website/js/script.js
@@ -1,3 +1,18 @@
 function setDirection(direction) {
     document.documentElement.setAttribute("dir", direction);
 }
+
+window.onload = function () {
+
+    const savedDirection = localStorage.getItem("direction");
+
+    if (savedDirection) {
+        document.documentElement.setAttribute("dir", savedDirection);
+    }
+};
+
+function setDirection(direction) {
+    alert("Button wurde geklickt!");
+
+    document.documentElement.setAttribute("dir", direction);
+}
